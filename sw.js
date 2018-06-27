70f5bc6d29ff78bcce73e5187f182ef39eca5d61
Aniuchi Adaobi <Aniuchi Adaobi>
2018-06-27 15:15:10 +0100

    made changes to the install event

--- sw.js
+++ sw.js
@@ -1,28 +1,18 @@
-const staticAssets = [
-    './',
-    './css/bootstrap.min.css',
-    './css/custom.css',
-    './js/jquery.js',
-    './js/bootstrap.min.js',
-    './js/main.js'
-];
-const staticCacheName = 'currency-converter-static-v2';
+const staticCacheName = 'converter-static-v1';
 
-self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(staticCacheName)
-        .then( function(cache){
-            return cache.addAll(
-                [
-                    './',
-                    './css/bootstrap.min.css',
-                    './css/custom.css',
-                    './js/jquery.js',
-                    './js/bootstrap.min.js',
-                    './js/main.js'
-                ]  
-            );
-        })
-    );
-    
-}); 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(staticCacheName).then( cache => {
+      return cache.addAll([
+        '/skeleton',
+        'js/jquery.js',
+        'js/bootstrap.min.js',
+        'js/main.js',
+        'css/custom.css',
+        'css/bootstrap.min.css',
+        'imgs/icon.png',
+        'https://fonts.googleapis.com/css?family=Roboto:400,500,700'
+      ]);
+    })
+  );
+});
