1da83fa22c72a511b0b80af9ca8e581ed315bf9b
Aniuchi Adaobi <Aniuchi Adaobi>
2018-06-27 16:45:54 +0100

    changes to service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const app_prefix = 'CurrencyConverter_';
-const version = 'version_01';    
+const version = 'version_02';    
 const cache_name = `${app_prefix}${version}`;
 const repository = 'my-currency-converter';
 const assets = [                            
@@ -13,20 +13,31 @@ const assets = [
   `https://fonts.googleapis.com/css?family=Roboto:400,500,700`  
 ];
 
-
-self.addEventListener('install', event => {
-    console.log(assets);
- /*  event.waitUntil(
-    caches.open(staticCacheName).then( cache => {
-      return cache.addAll([
-        './',
-        './assets/js/jquery.js',
-        './assets/js/bootstrap.min.js',
-        './assets/js/main.js',
-        './assets/css/custom.css',
-        './assets/css/bootstrap.min.css',
-        'https://fonts.googleapis.com/css?family=Roboto:400,500,700'
-      ]);
-    })
-  ); */
-});
+// Respond with cached resources
+self.addEventListener('fetch', event => {
+    console.log('fetch request : ' + event.request.url)
+    event.respondWith(
+      caches.match(event.request).then( request => {
+        if (request) { // if cache is available, respond with cache
+          console.log('responding with cache : ' + event.request.url)
+          return request
+        } else {       // if there are no cache, try fetching request
+          console.log('file is not cached, fetching : ' + event.request.url)
+          return fetch(event.request)
+        }
+  
+        // You can omit if/else for console.log & put one line below like this too.
+        // return request || fetch(e.request)
+      })
+    )
+  })
+  
+  // Cache resources
+  self.addEventListener('install', event => {
+    event.waitUntil(
+      caches.open(cache_name).then(cache => {
+        console.log('installing cache : ' + cache_name)
+        return cache.addAll(assets)
+      })
+    )
+  });
