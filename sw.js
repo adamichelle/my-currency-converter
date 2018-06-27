2010c9483db19b3da3e4d76ca8af84762c1c9956
Aniuchi Adaobi <Aniuchi Adaobi>
2018-06-27 17:05:47 +0100

    update fetch event

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const app_prefix = 'CurrencyConverter_';
-const version = 'version_02';    
+const version = 'version_01';    
 const cache_name = `${app_prefix}${version}`;
 const repository = 'my-currency-converter';
 const assets = [                            
@@ -18,15 +18,14 @@ self.addEventListener('fetch', event => {
     console.log('fetch request : ' + event.request.url)
     event.respondWith(
       caches.match(event.request).then( request => {
-        /* if (request) { // if cache is available, respond with cache
+        /* //This was to check if it was working
+        if (request) { // if cache is available, respond with cache
           console.log('responding with cache : ' + event.request.url)
           return request
         } else {       // if there are no cache, try fetching request
           console.log('file is not cached, fetching : ' + event.request.url)
           return fetch(event.request)
         } */
-  
-        // You can omit if/else for console.log & put one line below like this too.
         return request || fetch(event.request);
       })
     )
