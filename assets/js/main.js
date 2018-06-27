6b3818964fa350509229494a60330498f639453a
Aniuchi Adaobi <Aniuchi Adaobi>
2018-06-27 15:30:43 +0100

    changed the location of the sw filw

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,7 +59,7 @@ function getAllCurrencies() {
 
 function registerServiceWorker() {
     if(!navigator.serviceWorker) return;
-    navigator.serviceWorker.register('./sw.js', {scope: '/'})
+    navigator.serviceWorker.register('sw.js')
     .then( function(){
         console.log("Registered");
     })
