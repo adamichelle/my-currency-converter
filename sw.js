192ea0bf4dd716d283b22fae898706e9c9321ccd
Aniuchi Adaobi <Aniuchi Adaobi>
2018-06-27 16:34:49 +0100

    changes

--- sw.js
+++ sw.js
@@ -1,7 +1,22 @@
-const staticCacheName = 'converter-static-v1';
+const app_prefix = 'CurrencyConverter_';
+const version = 'version_01';    
+const cache_name = `${app_prefix}${version}`;
+const repository = 'my-currency-converter';
+const assets = [                            
+  `/${repository}/`,                     
+  `/${repository}/index.html`,
+  `/${repository}/assets/js/jquery.js`,
+  `/${repository}/assets/js/bootstrap.min.js`,
+  `/${repository}/assets/js/main.js`,
+  `/${repository}/assets/css/bootstrap.min.css`, 
+  `/${repository}/assets/css/custom.css`,
+  `https://fonts.googleapis.com/css?family=Roboto:400,500,700`  
+]
+
 
 self.addEventListener('install', event => {
-  event.waitUntil(
+    console.log(assets);
+ /*  event.waitUntil(
     caches.open(staticCacheName).then( cache => {
       return cache.addAll([
         './',
@@ -13,5 +28,5 @@ self.addEventListener('install', event => {
         'https://fonts.googleapis.com/css?family=Roboto:400,500,700'
       ]);
     })
-  );
+  ); */
 });
