81ff1a3fbadf15111efd4da360862399c7b5e59d
Aniuchi Adaobi <Aniuchi Adaobi>
2018-06-27 15:44:28 +0100

    changes

--- sw.js
+++ sw.js
@@ -4,13 +4,7 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(staticCacheName).then( cache => {
       return cache.addAll([
-        './',
-        './assets/js/jquery.js',
-        './assets/js/bootstrap.min.js',
-        './assets/js/main.js',
-        './assets/css/custom.css',
-        './assets/css/bootstrap.min.css',
-        'https://fonts.googleapis.com/css?family=Roboto:400,500,700'
+        './'
       ]);
     })
   );
